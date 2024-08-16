1e87915b1ff51fe1304745aa967b516750941a11
Name <email@example.ru>
2024-08-17 00:04:00 +0300

    12.1.5 (HW-03)

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -41,12 +41,12 @@ class Program
     static void ShowAds()
     {
         Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
-        Thread.Sleep(1000);
-
-        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
         Thread.Sleep(2000);
 
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(4000);
+
         Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
-        Thread.Sleep(3000);
+        Thread.Sleep(6000);
     }
 }
